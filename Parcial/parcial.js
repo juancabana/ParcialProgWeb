cff15483408d3552808256dba3a3ba382b4a8a83
juancabana <juand.cabanat@unac.edu.co>
2022-05-10 16:23:01 -0500

    commit final

--- Parcial/parcial.js
+++ Parcial/parcial.js
@@ -2,26 +2,31 @@ const input = document.getElementById("input");
 
 function calc() {
   const content = input.value;
-
   const val = content.split(" ");
   let ver = true;
 
-  (principal = 0), (secondary = 0);
   for (i = 0; i < val.length; i++) {
-    for (j = 0; j < val.length; j++) {
-      // Condition for principal
-      // diagonal
-      if (i == j) principal += parseInt(val[i][j]);
-
-      // Condition for secondary
-      // diagonal
-      if (i + j == val.length - 1) secondary += parseInt(val[i][j]);
+    if (val[i].length != val.length) {
+      ver = false;
+      break;
     }
   }
-  const abs = Math.abs(principal - secondary);
 
-  console.log(principal);
-  console.log(secondary);
+  if (ver) {
+    (principal = 0), (secondary = 0);
+    for (i = 0; i < val.length; i++) {
+      for (j = 0; j < val.length; j++) {
+        if (i == j) principal += parseInt(val[i][j]);
 
-  alert("El resultado es " + abs);
+        if (i + j == val.length - 1) secondary += parseInt(val[i][j]);
+      }
+    }
+
+    const abs = Math.abs(principal - secondary);
+    console.log(principal);
+    console.log(secondary);
+    alert("El resultado es " + abs);
+  } else {
+    alert("La matriz no es cuadrada");
+  }
 }
